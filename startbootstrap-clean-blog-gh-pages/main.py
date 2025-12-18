ed332081395737f220092cf22b3db7df04a9ea30
Ankitha <ankithajv1285@gmail.com>
2025-12-18 11:07:35 +0530

    Made Contact form work

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -49,9 +49,9 @@ def contact():
 
 
 def send_email(name , email , phone , message):
-    email_address = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
 
-    with smtplib.SMTP("smtp.gmai.com") as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(OWN_EMAIL,OWN_PASSWORD)
         connection.sendmail(OWN_EMAIL,OWN_EMAIL,email_message)
